d52845756c0df9137234accb7e990e13bf4f84ae
Marius Hillenbrand <marius.hillenbrand@kit.edu>
2016-07-19 18:08:42 +0200

    complete test code and alternative generator, add sample run

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
-#include <iostream>
+#include <algorithm>
 #include <cinttypes>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <typeinfo>
 
 #include "benchmark_kernels.h"
 #include "common.h"
@@ -25,25 +29,108 @@ namespace xmem {
     bool g_verbose = false; /* needed by X-Mem source code */
 };
 
-/* TODO templateify this and add wrapper classes */
-float testGenerator(int N, bool printHist) {
+class permutated_list_generator {
+protected:
+    int N;
+    uint64_t * list;
 
-    uint64_t *array =  new uint64_t[N];
+public:
+    permutated_list_generator(int N) : N(N) {
+	list = new uint64_t[N];
+    }
 
+    virtual ~permutated_list_generator() {
+	delete list;
+    }
+
+    int getN() const { return N; }
+
+    virtual uint64_t * getlist() = 0;
+};
+
+class XMem_list_generator : public permutated_list_generator {
+
+public:
+    XMem_list_generator(int N) : permutated_list_generator(N) { }
+
+    uint64_t * getlist() {
+
+	xmem::build_random_pointer_permutation(list,
+	    (void *) &list[N-1], xmem::CHUNK_64b);
+
+	return list;
+    }
+
+};
+
+class external_shuffle_generator : public permutated_list_generator {
+
+public:
+    external_shuffle_generator(int N) : permutated_list_generator(N) { }
+
+    uint64_t * getlist() {
+
+	/* represent the order in which we are going to traverse the
+	 * pointer nodes */
+	int * traversal_order = new int[N + 1];
+
+	/* 0, 1, 2, ...., N-1 */
+	for(int i = 0; i < N; i++)
+	    traversal_order[i] = i;
+
+	/* and back to 0 */
+	traversal_order[N] = 0;
+
+	/* at this point, traversal_order represents a hamiltonion
+	 * cycle, albeit a not very random one */
+
+	std::mt19937_64 gen(time(NULL)); //Mersenne Twister random number generator, seeded as in X-Mem
+	//std::minstd_rand0 gen(time(NULL)); // slightly faster alternative
+
+	/* now we randomize the order in which we visit the nodes,
+	 * maintaining the invariants that
+	 *   - we begin and end at the 0
+	 *   - traversal_order represents a hamiltonian cycle over all
+	 *     nodes 
+	 * For that purpose, we only shuffle the elements 1, 2, ..., N-1
+	 * in the middle */
+	std::shuffle(traversal_order + 1, traversal_order + (N - 1), gen);
+
+	/* std::cout << std::endl;
+
+	for(int i = 0; i < N + 1; i++)
+		std::cout << traversal_order[i] << " ";
+
+	std::cout << std::endl; */
+
+	/* implement the traversal order within the pointer array */
+	for(int i = 0; i < N; i++) {
+	    /* std::cout << traversal_order[i] << " -> ";
+	    std::cout << traversal_order[i+1] << std::endl; */
+	    list[ traversal_order[i] ] =
+		(uint64_t) &list[ traversal_order[i+1] ];
+	}
+
+	delete traversal_order;
+
+	return list;
+    }
+
+};
+
+template<class T> float testGenerator(int N, bool printHist) {
+
+    T gen(N);
     int * strides = new int[2*N];
     int i;
 
     for(i = 0; i < 2 * N; i++)
 	strides[i] = 0;
 
-    std::cout << "list of " << N << " elements." << std::endl;
+    std::cout << typeid(T).name() << ": list of " << N << " elements." << std::endl;
 
-    xmem::build_random_pointer_permutation(array,
-	    (void *) &array[N-1], xmem::CHUNK_64b);
-
-    std::cout << "shuffled." << std::endl;
-
-    uint64_t * p = array;
+    uint64_t * list = gen.getlist();
+    uint64_t * p = list;
 
     int cyclelength = 0;
     bool * visited = new bool[N];
@@ -52,9 +139,9 @@ float testGenerator(int N, bool printHist) {
 	visited[i] = false;
 
     do {
-	/* std::cout << " " << (p - array) << ","; */
+	/* std::cout << " " << (p - list) << ","; */
 
-	visited[ p - array ] = true;
+	visited[ p - list ] = true;
 
 	cyclelength ++;
 
@@ -62,23 +149,22 @@ float testGenerator(int N, bool printHist) {
 
 	/* Note: when coding undercaffeinated, you might need to resort
 	 * to this kind of debugging:
-	 std::cout << (p - array) << " -> " << ( (uint64_t *)*p - array)
+	 std::cout << (p - list) << " -> " << ( (uint64_t *)*p - list)
 		<< ": " << stride << " idx " << (N + stride) << std::endl;
-	*/
+	 */
 
 	strides[ N + stride ] ++;
 
 	p = (uint64_t *) *p;
 
-    } while( ! visited[ p - array ] );
+    } while( ! visited[ p - list ] );
 
-    std::cout << "found cycle of length " << cyclelength;
+    std::cout << typeid(T).name() << ": found cycle of length " << cyclelength;
     float covered = (100.0* cyclelength) / N;
     std::cout << " (i.e., covering " << covered << "%)";
-    std::cout << " on index " << (p - array) << std::endl;
+    std::cout << " on index " << (p - list) << std::endl;
 
     std::cout << std::endl;
-
 
     if(printHist) {
 	/* simple early version
@@ -109,7 +195,8 @@ float testGenerator(int N, bool printHist) {
 	}
 
 	for(i = 0; i < 20; i++) {
-	    std::cout << "[" << a[i] << ";" << b[i] << ")  ";
+	    std::cout <<  "[" << std::setw(9) << a[i] << ";";
+	    std::cout << std::setw(9) << b[i] << ") ";
 
 	    int dots = 40 * hist[i] / maxAmount;
 
@@ -121,21 +208,44 @@ float testGenerator(int N, bool printHist) {
 
 	    std::cout << " (" << hist[i] << ")" << std::endl;
 	}
+
+	std::cout << std::endl;
     }
 
-    delete array;
     delete strides;
     delete visited;
 
     return covered;
 }
 
+template<class T> void speedrun(int N) {
+
+    T gen(N);
+
+    uint64_t * l = gen.getlist();
+
+    (void) l;
+
+}
+
 int main(int argc, char ** argv) {
 
-    testGenerator(128, true);
-    testGenerator(1024, true);
+    //testGenerator<external_shuffle_generator>(32<<20, true);
+    //speedrun<XMem_list_generator>(32 << 20);
+    //speedrun<external_shuffle_generator>(32 << 20);
 
-    testGenerator(6<<20, true);
+    testGenerator<external_shuffle_generator>(128, true);
+    testGenerator<external_shuffle_generator>(1024, true);
+    testGenerator<external_shuffle_generator>(6<<20, true);
+    testGenerator<external_shuffle_generator>(32<<20, true);
+
+    testGenerator<XMem_list_generator>(128, true);
+    testGenerator<XMem_list_generator>(1024, true);
+
+
+    testGenerator<XMem_list_generator>(6 << 20, true);
+    testGenerator<XMem_list_generator>(32 << 20, true);
+    /* testGenerator(6<<20, true); */
     /* testGenerator(20<<20, true);
     testGenerator(32<<20, true);
     testGenerator(64<<20, true); */
@@ -144,11 +254,20 @@ int main(int argc, char ** argv) {
 
     float sum = 0;
     for(int i = 0; i < 100; i++)
-	sum += testGenerator(4<<20, true);
+	sum += testGenerator<XMem_list_generator>(2 << 20, false);
 
     sum /= 100;
-    std::cout << std::endl << "for 4 MiB test case, average coverage of ";
+
+    float sum2 = 0;
+    for(int i = 0; i < 100; i++)
+	sum2 += testGenerator<external_shuffle_generator>(2 << 20, false);
+
+    sum2 /= 100;
+
+    std::cout << std::endl << "X-Mem: for 2 MiB test case, average coverage of ";
     std::cout << sum << "% (100 runs)" << std::endl;
 
+    std::cout << std::endl << "external shuffle: for 2 MiB test case, average coverage of ";
+    std::cout << sum2 << "% (100 runs)" << std::endl;
 }
 
